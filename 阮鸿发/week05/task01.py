8e43e2260b0b7b50d518d1e64ed7e9ad9ce75538
AUSTRUAN <1004671047@qq.com>
2025-09-26 22:48:11 +0800

    Create task01.py

---
+++ 阮鸿发/week05/task01.py
@@ -0,0 +1,19 @@
+import openai
+import json
+from openai import OpenAI
+
+# https://bailian.console.aliyun.com/?tab=api#/api/?type=model&url=2712576
+client = openai.OpenAI(
+    api_key="sk-2f0f4d94c5d349a3a74830a5e4f4da15", # https://bailian.console.aliyun.com/?tab=model#/api-key
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+)
+
+completion = client.chat.completions.create(
+    # 模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+    model="qwen-plus",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "你能写个睡前故事吗？"}
+    ],
+)
+print(completion.model_dump_json(),end=" ", flush=True)
